1195c13255cfac619046f9401abbeb93b87e8c1e
afbdpk <afbdpk@pc9w20.inf.elte.hu>
2018-09-24 10:52:39 +0200

    Issue controller: PUT and DELETE methods

--- src/main/java/hu/elte/issuetrackerrest/controllers/IssueController.java
+++ src/main/java/hu/elte/issuetrackerrest/controllers/IssueController.java
@@ -5,12 +5,14 @@ import hu.elte.issuetrackerrest.repositories.IssueRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/issues")
@@ -38,5 +40,27 @@ public class IssueController {
     public ResponseEntity<Issue> post(@RequestBody Issue issue) {
         Issue savedIssue = issueRepository.save(issue);
         return ResponseEntity.ok(savedIssue);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Issue> delete(@PathVariable Integer id) {
+        Optional<Issue> oIssue = issueRepository.findById(id);
+        if (oIssue.isPresent()) {
+            issueRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Issue> update(@PathVariable Integer id, @RequestBody Issue issue) {
+        Optional<Issue> oIssue = issueRepository.findById(id);
+        if (oIssue.isPresent()) {
+            issue.setId(id);
+            return ResponseEntity.ok(issueRepository.save(issue));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
